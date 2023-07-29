0c0b3b7f79b37aa22caa06041759e3643dc9eab7
Yuri Cruz <yuricruzf@gmail.com>
2023-07-29 17:15:23 -0300

    Variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  //Variáveis
+  const name: string = "Yuri";
+  const age: number = 24;
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
       <h1>React com typeScript</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando</p>}
     </div>
   );
 }
