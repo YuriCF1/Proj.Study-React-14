835eb2920d992bc25b10536fe2398987d749c0a9
Yuri Cruz <yuricruzf@gmail.com>
2023-07-31 13:49:26 -0300

    Creating component

---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const FirstComponent = (props: Props) => {
+  return (
+    <div>Meu primeiro componente</div>
+  )
+}
+
+export default FirstComponent
